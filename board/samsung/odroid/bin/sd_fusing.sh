e79de69d96bf531f21ad4e39ebea71c119c5dc03
Dongjin Kim <tobetter@gmail.com>
2018-03-27 20:05:27 -0400

    ODROID: XU3/4: add prebuilt binaries
    Change-Id: I95297c30440c5a3d565a935048ba854fd00fd188
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>

---
+++ board/samsung/odroid/bin/sd_fusing.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Copyright (C) Dongjin Kim <tobetter@gmail.com>
+#
+# SPDX-License-Identifier:	GPL-2.0+
+#
+
+set -x
+
+. $(dirname "$0")/functions
+
+dev="$1"
+
+[ -z ${binary_dir} ] && binary_dir="$(dirname $0)"
+bl1=${binary_dir}/bl1.bin.hardkernel
+bl2=${binary_dir}/bl2.bin.hardkernel
+uboot=${binary_dir}/u-boot.bin
+
+flash_bootloader ${bl1} ${bl2} ${uboot} ${dev}
+eject $1
+echo Finished.
