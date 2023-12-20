2caf9d476260b724b8667fb97f9159dd0b559d5d
Dongjin Kim <tobetter@gmail.com>
2023-12-20 18:18:12 +0900

    Initial commit
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>
    Change-Id: If489b0c3510d7a1603262a677d34acb11550a8d7

---
+++ usr/lib/u-boot/odroidc5/sd_fusing.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Copyright (C) 2018 Hardkernel Co,. Ltd
+# Dongjin Kim <tobetter@gmail.com>
+#
+# SPDX-License-Identifier:      GPL-2.0+
+#
+
+abort() {
+	echo "$1"
+	exit 1
+}
+
+[ -z "$1" ] && abort "usage: $0 <your/memory/card/device>"
+[ -z "$UBOOT" ] && UBOOT="${PWD}/u-boot.bin"
+if [ ! -f "$UBOOT" ] ; then
+	UBOOT="$(echo "$0" | perl -pe 's/[^\/]*$//g')u-boot.bin"
+fi
+[ ! -f "$UBOOT" ] && abort "error: $UBOOT is not exist"
+
+sudo dd if="$UBOOT" of="$1" conv=fsync,notrunc bs=512 seek=1
+
+sync
+
+echo "Finished."
